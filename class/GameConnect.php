61fa5b87c696d9cc15dcf96870be84261e53b02c
Donata Małecka <niphree@gmail.com>
2007-08-18 06:44:26 -0400

    added connection options
    via git-CVS emulator

--- class/GameConnect.php
+++ class/GameConnect.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
-	
-	:/ don't know if it work's :/
-
-*/
 
 class GameConnect{
 
@@ -15,22 +10,45 @@ class GameConnect{
 	public function __construct($host, $port)
 	{
 		//prabobly to change\
-		echo "connecting<br />";
-		 $this->socket =& socket_create(AF_INET, SOCK_STREAM, 0);// or die("Could not create socket\n");
-		 socket_connect($this->socket, $host, $port) ;//or die("Could not connect to server\n");
-		echo "connected<br />";
+		//echo "connecting<br />";
+		 $this->socket =& socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
+		 socket_set_option( $this->socket,
+		 	SOL_SOCKET,  // socket level
+			SO_RCVTIMEO, // timeout option
+			array(
+				"sec"=>10, // Timeout in seconds
+				"usec"=>0  // I assume timeout in microseconds
+			)
+		 );
+
+		 socket_connect($this->socket, $host, $port) or die("Could not connect to server\n");
+		//echo "connected<br />";
 	}
 	
 	public function getMessage($length)
 	{
-		$msg = socket_read($this->socket, $length)  or die("Could not send data to server\n");
+		if ( false === ($msg = socket_read($this->socket, $length)) )
+		{
+			socket_close($this->socket->socket_close); 
+			die("Could not send data to server\n"); 
+		};
 		return $msg;
 	}
 	
 	public function sendMessage($message)
 	{
-		socket_write($this->socket,$message) or die("Could not read server response\n");
+		if ( false === (socket_write($this->socket,$message) ))
+		{
+			socket_close($this->socket); 
+			die("Could not read server response\n"); 
+		};
 	}
+	
+	public function disconnect()
+	{
+		socket_close($this->socket); 
+	}
+
 	
 }
 
