47b6236881b7cd80ab21fde4c77a4c1ecb41615b
Tim Ansell <mithro@mithis.com>
2007-08-27 17:56:38 +0930

    Removed excess whitespace and added a FIXME.

--- rss.php
+++ rss.php
@@ -1,15 +1,8 @@
 <?php
 
-
 require_once "DB.php";
 require_once "class/Backend.php";
 require_once "class/Rss.php";
 
-
-
+// FIXME: This isn't going to work everywhere..
 Rss::createRss('http://www.tarl.org/~niphree/parsec/');
-
-
-
-
-?>
