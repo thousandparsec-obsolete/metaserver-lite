4607e664e4c32a32d8ecb80861327b685ba3e5de
Tim Ansell <mithro@mithis.com>
2007-08-27 17:38:10 +0930

    Slight cleanup of index.php

--- index.php
+++ index.php
@@ -1,32 +1,20 @@
 <?php
-   
-  /*
-  test
-  */
-   
+
   require_once "DB.php";
   require_once "class/Backend.php";
-   
-  //require_once "libtpproto-php/Frame.php";
-   
-   
-   
+  
+  // FIXME: This should be in config.php 
   //$dsn = "mysqli://metaserver:meatereater@localhost/metaserver";
-   
-  //
-  $dsn = "mysqli://intart2_8:parsec@sql.intart2.nazwa.pl:3305/intart2_8";
+  $dsn  = "mysqli://intart2_8:parsec@sql.intart2.nazwa.pl:3305/intart2_8";
    
   $time = time();
+  $db   = new Backend($dsn, $time);
    
-  $db = new Backend($dsn, $time);
-   
-   
-  // to consider: change switch for some kind of class/function responsible for all action
-   
+  // TODO: to consider: change switch for some kind of class/function responsible for all action
   switch ($_REQUEST['action'])
   {
     case 'update':
-    	include ("actions/update.php");
+      include ("actions/update.php");
     break;
     case 'get':
       include ("actions/get.php");
